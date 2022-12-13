29bc2862d279d8287a1e8743dea30491fb4fd3e7
Warfa <warfahassan35@yahoo.com>
2022-12-12 23:38:36 -0500

    fixed event listener

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 // 
 
 var apiKey = "d2e8a2ba66ffa64874aae0ac89d15d3c";
-var currentCity="Columbus";
+var searchCityEl = document.getElementById("searchCity");
 
 
-apiURL= "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&APPID=" + apiKey;
+
 
 var timeDisplayEl = $('#currentDay');
 
@@ -18,8 +18,12 @@ function displayTime() {
 displayTime();
 setInterval(displayTime, 1000);
 
-// Console logs API in the console log
-fetch(apiURL, {
+
+$(".search-button").on("click", function() {
+ var currentCity = searchCityEl.value;
+  apiURL= "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&APPID=" + apiKey;
+
+  fetch(apiURL, {
     // The browser fetches the resource from the remote server without first looking in the cache.
     // The browser will then update the cache with the downloaded resource.
     cache: 'reload',
@@ -35,4 +39,18 @@ fetch(apiURL, {
     //   Testing Fetching API city name
        console.log(data.name);
     });
+    // testing search button
+    console.log(currentCity);
+
+});
+
+$(".clear-history").on("click", function() {
+    // testing search button
+    console.log("clear history button works");
+
+});
+
+
+// Console logs API in the console log
+
 
